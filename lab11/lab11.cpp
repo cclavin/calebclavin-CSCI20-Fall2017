d27140616d6236d319f3cb126065a23a5332e4a4
Caleb Clavin <cclavin001@student.butte.edu>
2017-08-25 02:09:04 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,30 @@
+The computer randomly selects a number between 1-100
+The user begins with 10 points
+
+The user guesses a number between 1-100
+
+If the guessed number is higher than the selected number, then the program
+  indicates to guess lower and the user may not guess higher than their 
+  guessed number
+If the guessed number is lower than the selected number, then the program
+  indicates to guess higher and the user may not guess lower than their 
+  guessed number
+If the guessed number is correct, then the user does not lose points and the
+  total points left is displayed.
+  
+After a failed guess the user loses 1 point
+
+Repeat lines 7-16 until the user guesses correctly or until there are 0 points 
+  remaining
+
+
+
+  
+
+  
+
+
+
+
+
+
