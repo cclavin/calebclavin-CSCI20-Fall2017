b4c92a18ebe3683f610d58111cbc3de45524cef0
Caleb Clavin <cclavin001@student.butte.edu>
2017-10-11 05:42:45 +0000

    Labsubmission 3.1

---
+++ lab31/lab31.cpp
@@ -0,0 +1,46 @@
+//Created by: Caleb Clavin
+//Created on: 10/10/2017
+
+//Title
+
+#include <iostream>
+using namespace std;
+
+
+
+
+int main() 
+{
+    int num_phones = 0;
+    int num_tablets = 0;
+    int gb_data = 0;
+    
+    cout << "To find the best plan, please enter the amount of phones to be used: ";
+    cin >> num_phones;
+    
+    cout << "Enter the amount of tablets: ";
+    cin >> num_tablets;
+    
+    cout << "Enter the amount of data to be used: ";
+    cin >> gb_data;
+    
+    if((gb_data > 25)&&(num_phones <= 3)&&(num_tablets == 0)) {
+        cout << "The best plan for you is the Sprint Unlimited.";
+    }
+    else if((gb_data < 25)&&(gb_data != 2)||(gb_data != 0)){
+        cout << "The best plan for you is the ATT Family Plan.";
+    }
+    if ((gb_data == 2)&&(num_tablets == 0)) {
+        cout << "The best plan for you is the Sprint 2GB Plan.";
+    }
+    else if ((num_tablets > 0)&&(gb_data == 2)) {
+        cout << "The best plan for you is the ATT 3GB Family Plan.";
+    }
+    if ((num_phones > 3)&&(gb_data > 25)) {
+        cout << "The ATT Unlimited Choice plan is the best for your situation.";
+    }
+    
+    
+    
+    
+}
