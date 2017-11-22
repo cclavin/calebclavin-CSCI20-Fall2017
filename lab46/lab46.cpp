8314298f659adc434d8eb1168164783b06c9a801
Caleb Clavin <cclavin001@student.butte.edu>
2017-11-22 06:30:20 +0000

    Lab 4.6 Submission

--- lab46/lab46.cpp
+++ lab46/lab46.cpp
@@ -1,7 +1,7 @@
 //Created by: Caleb Clavin
 //Created on: 11/16/2017
 
-//Grades
+//GPA Calculator and transcript output- This program reads input from a file and calculates students' gpa by using the fstream library and basic file input/output.
 
 #include <iostream>
 #include <string>
@@ -13,18 +13,19 @@ using namespace std;
 int main() {
    ifstream fin;
    ofstream fout;
-   string first_name;
-   string last_name;
-   string buffer;
-   string class_data;
-   string credits;
-   char classes[10];
    
    int i = 0;
    int num_classes;
-   int grades;
-   int total_credits;
+   int num_students = 1;//can be modified depending on the amount of students needed to calculate for
+   char grades[num_classes];
+   char classes[num_classes];
+   double total_credits = 0;
+   double grade_points = 0;
+   double gpa = 0;
+   double credits[num_classes];
    
+   string first_name;
+   string last_name;
    
    
    fin.open("grades.txt");
@@ -33,35 +34,64 @@ int main() {
       cout << "Could not open file..." << endl;      
    }
  
-   while(!fin.eof()) {
+   for(i = 0; i < num_students; ++i) {//This loop will repeat, depending on the number of students needed to calculate.
       fin >> first_name;
    
       fin >> last_name;
      
       fin >> num_classes;
-   
-      for(i = 0; i < num_classes * 2; ++i) {//The loop iterates twice for every class for grade and the amount of credits
-         fin >> classes[i];
-      }//Stuck on trying to figure out how to get the data from the string
+      
+      for(i = 0; i < num_classes; ++i) {//The loop iterates twice for every class in order to read in grade and the amount of credits
+         fin >> credits[i];
+         fin >> grades[i];
+      }
+      for(i = 0; i < 10; ++i) {//Checks for each grade letter and adds the points accordingly
+         if(grades[i] == 'A') {
+            grade_points += 4;
+         }
+         else if(grades[i] == 'B') {
+            grade_points += 3;
+         }
+         else if(grades[i] == 'C') {
+            grade_points += 2;
+         }
+         else if(grades[i] == 'D') {
+            grade_points += 1;
+         }
+         else if(grades[i] == 'F') {
+            grade_points += 0;
+         }
+      }
+         for(i = 0; i < num_classes; ++i) {
+            total_credits = total_credits + credits[i];//The credits are added up in order to make the gpa calculation
+         }   
+         
+         gpa = grade_points / total_credits;
+         
+      //Finally, the calulation is complete and a new stream is opened to output to a new html file.
+      fout.open("reportcard.html");
+      fout << "<!DOCTYPE html>" << endl;
+      fout << "<html>" << endl;
+      fout << "<body>" << endl;
+      fout << "<h1><b>" << first_name << " " << last_name << "</b></h1>" << endl;
+      fout << "<p> Student GPA: " << gpa << "</p>" << endl;
+      fout << "</body>" << endl;
+      fout << "</html>" << endl;
+         
+      
+         
+      
    }
-   class_data = classes;
-   
-   if(classes ==) {
-      credits = classes;
-   }
+  
    
    
    
    
-   cout << credits;
-   while(fin.good()) {
-   fin >> classes;
-   cout << credits; 
-  }
    
+  
   
   fin.close();
-  
+  fout.close();
   
   
 }
