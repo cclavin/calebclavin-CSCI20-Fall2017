1ac5a068b4715dfbd67d234351c9a3d8da84662a
Caleb Clavin <cclavin001@student.butte.edu>
2017-12-01 03:28:28 +0000

    Lab 5.1 Submission

--- lab51/lab51.cpp
+++ lab51/lab51.cpp
@@ -3,23 +3,23 @@
 #include <cstring> 
 using namespace std;   
 
-int main() {
-    int nameLength = 0;
+int main() {//Declares the main function and opens for its definition
+    int nameLength = 0;//Creates int variable nameLength and assigns to it 0
     char * name = new char[1000]; //Declares a char pointer called name and attempts to create an array of pointers
                             //it points to the whole array
 
-    cout << "Please enter a 10 letter word or less" << endl; 
-    cin >> name; 
+    cout << "Please enter a 10 letter word or less" << endl; //Outputs the string of text and finishes with an newline
+    cin >> name; //User enters their name into the array
 
     char * head  = name; //Points head to the first letter of the string
     char * tail = name; //Also points tail to the first letter of the string
-    nameLength = strlen(name);//Stores the length of the array name points to, by deleting the dereference operator, it can find the length of the entire string 
+    nameLength = strlen(*name);//Stores the length of the array name points to, the dereference operator should be deleted in order to get the length of the whole array
 
     cout << "Your word is " << name << endl;//firststr is not declared and has no value stored
 
-    if (nameLength<10)
+    if (nameLength<10)//Sets condition allowing the body to be read only if the string is under 10 characters
     {
-         while (*head != '\0')
+         while (*head != '\0')//While the pointee pointed to by head is not the null character...it does this in order to stop looping once it reaches the end of the string
          {
                 cout << *head;//Outputs  the data pointed to by head until a null character is found by ++head, by adding after each iteration it cycles through the array name which it is 
                 head++; //it cycles through the array name which it is pointing
@@ -27,14 +27,14 @@ int main() {
      }
     else//else if namelength is greater than 10
     {
-        cout << "WARNING WORD TOO BIG TO DISPLAY!!" << endl;
+        cout << "WARNING WORD TOO BIG TO DISPLAY!!" << endl;//Outputs a string of text warning the user if their string is too long
     }
 
-     cout << endl;
+     cout << endl;//Adds a newline
 
      tail = &name[strlen(name) - 1]; //Points tail to the last letter of the string that is input
 
-     if (nameLength < 10)
+     if (nameLength < 10)//Again, ensures the code below is read only if the string is under ten characters
      {
           while (tail != name)//While tail doesn't equal the first character
           {
@@ -42,7 +42,7 @@ int main() {
                  tail--;
           }
      }
-     cout << endl;
+     cout << endl;//Adds another newline for readability
 
      head = name;//Points to the first character again
      tail = &name[strlen(name) - 1];//Points tail to the last character again
@@ -51,10 +51,10 @@ int main() {
      tail--;//Goes backwards from the last character to the first
 
      if (*head == *tail)//Compares the backwards string that tail points to and the regular string pointed to by head
-     {
-         cout << "It is a palindrome!" << endl;
-     }
-     else
+     {              
+         cout << "It is a palindrome!" << endl;//The text is output if the array is found to be the same forwards and backwards through comparing...
+     }                                      //the characters of the array, read forwards by pointer head and read backwards by pointer tail
+     else//Indicates that the string is not a palindrome because the strings, both forwards and backwards, are not equivalent
      {
          cout << "It is not an palindrome" << endl;
      }
